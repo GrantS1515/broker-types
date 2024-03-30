a9dc2dfc20f98c81799455e2060645360e67d0d7
W. Grant Schultheis <grantschultheis@gmail.com>
2024-03-30 18:13:40 -0400

    initial commit

---
+++ broker-types.ts
@@ -0,0 +1,53 @@
+export enum BrokerEventClass {
+	single = "broker__event__single",
+	getMultipleRequest = "broker__event__getMultipleRequest",
+	getMultipleResponse = "broker__event__getMultipleResponse",
+}
+
+export type SingleEvent = {
+	eventClass: BrokerEventClass
+	event: object,
+}
+
+export function newSingleEvent(event: object): SingleEvent {
+	return { eventClass: BrokerEventClass.single, event: event }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isSingleEvent(testObj: any): testObj is SingleEvent {
+	return (testObj.eventClass === BrokerEventClass.single)
+		&& (testObj.event !== undefined)
+}
+
+export type MultipleEventsRequest = {
+	eventClass: BrokerEventClass
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isMultipleEventsRequest(testObj: any): testObj is MultipleEventsRequest {
+	return (testObj.eventClass === BrokerEventClass.getMultipleRequest)
+}
+
+export type MultipleEventsResponse = {
+	eventClass: BrokerEventClass,
+	events: IndexedEvent[],
+}
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function isMultipleEventsResponse(testObj: any): testObj is MultipleEventsResponse {
+	return (testObj.eventClass === BrokerEventClass.getMultipleResponse) 
+		&& (testObj.events !== undefined)
+}
+
+export function newMultipleEventsResponse(events: IndexedEvent[]): MultipleEventsResponse {
+	return { eventClass: BrokerEventClass.getMultipleResponse, events: events }
+}
+
+export function newMultipleEventRequest(): MultipleEventsRequest {
+	return { eventClass: BrokerEventClass.getMultipleRequest }
+}
+
+export type IndexedEvent = {
+	index: number,
+	event: object,
+}
